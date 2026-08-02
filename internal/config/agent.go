77f561b2f2996c4ec1ef973aed406c92193253fa
cymiam <>
2026-08-02 22:46:53 +0300

    Fix poll interval

--- internal/config/agent.go
+++ internal/config/agent.go
@@ -15,6 +15,6 @@ var AgentConfig agentConfig
 func ParseAgentFlags() {
 	flag.StringVar(&AgentConfig.Addr, "a", ":8080", "address and port of server")
 	flag.Int64Var(&AgentConfig.PollInterval, "r", 10, "report interval")
-	flag.Int64Var(&AgentConfig.PollInterval, "p", 10, "poll interval")
+	flag.Int64Var(&AgentConfig.PollInterval, "p", 2, "poll interval")
 	flag.Parse()
 }
