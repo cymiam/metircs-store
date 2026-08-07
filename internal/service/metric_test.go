9a4b37896066826b091155a6a449f0aa78fd924c
cymiam <>
2026-08-07 20:50:18 +0300

    Added test for metric service and repository

---
+++ internal/service/metric_test.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMetricService_UpdateCounter(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		metricName string
+		newValue   int64
+	}{
+		{
+			name:       "Test Positive",
+			metricName: "test",
+			newValue:   2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			service := NewMetricService()
+			service.store.SetCounter("test", 1)
+			service.UpdateCounter(tt.metricName, tt.newValue)
+
+			expected := []int64{1, 3}
+			got, _ := service.GetCounter("test")
+
+			assert.Equal(t, expected, got)
+		})
+	}
+}
