d92ba57688b62dd32803c6cb722c9e8dad9aae57
cymiam <>
2026-08-12 23:06:28 +0300

    Split single test into groups

--- internal/handler/metric.go
+++ internal/handler/metric.go
@@ -6,8 +6,10 @@ import (
 	"strconv"
 
 	"github.com/cymiam/metircs-store/internal/logger"
+	models "github.com/cymiam/metircs-store/internal/model"
 	"github.com/cymiam/metircs-store/internal/service"
 	"github.com/go-chi/chi/v5"
+	"github.com/mailru/easyjson"
 	"go.uber.org/zap"
 )
 
@@ -46,7 +48,7 @@ func (handler *MetricHandler) HandleUpdate(w http.ResponseWriter, r *http.Reques
 			zap.Float64("MetricValue", metricValue))
 		w.WriteHeader(http.StatusOK)
 	default:
-		http.Error(w, "Неизвестный тип метрики", http.StatusBadRequest)
+		http.Error(w, fmt.Sprintf("Неизвестный тип метрики: %s", metricType), http.StatusBadRequest)
 	}
 }
 
@@ -54,6 +56,11 @@ func (handler *MetricHandler) HandleGetMetric(w http.ResponseWriter, r *http.Req
 	w.Header().Add("Content-type", "text/plain; charset=utf-8")
 	metricType := chi.URLParam(r, "metric_type")
 	metricName := chi.URLParam(r, "metric_name")
+
+	if metricName == "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
 	switch metricType {
 	case "counter":
 		value, ok := handler.metricService.GetCounter(metricName)
@@ -73,7 +80,7 @@ func (handler *MetricHandler) HandleGetMetric(w http.ResponseWriter, r *http.Req
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(fmt.Sprintf("%v", value)))
 	default:
-		http.Error(w, "Неизвестный тип метрики", http.StatusBadRequest)
+		http.Error(w, fmt.Sprintf("Неизвестный тип метрики: %s", metricType), http.StatusBadRequest)
 	}
 }
 
@@ -92,19 +99,92 @@ func (handler *MetricHandler) HandleGetMetrics(w http.ResponseWriter, r *http.Re
 	w.Write([]byte(body))
 }
 
+func (handler *MetricHandler) HandleUpdateJson(w http.ResponseWriter, r *http.Request) {
+
+	metric := models.Metrics{}
+	easyjson.UnmarshalFromReader(r.Body, &metric)
+	metricType := metric.MType
+	metricName := metric.ID
+
+	if metricName == "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	switch metricType {
+	case "counter":
+		handler.metricService.UpdateCounter(metricName, *metric.Delta)
+		logger.Log.Info("Update metric",
+			zap.String("MetricName", metricName),
+			zap.String("MetricType", metricType),
+			zap.Int("MetricValue", int(*metric.Delta)))
+		w.WriteHeader(http.StatusOK)
+	case "gauge":
+		handler.metricService.UpdateGauge(metricName, *metric.Value)
+		logger.Log.Info("Update metric",
+			zap.String("MetricName", metricName),
+			zap.String("MetricType", metricType),
+			zap.Float64("MetricValue", *metric.Value))
+		w.WriteHeader(http.StatusOK)
+	default:
+		logger.Log.Info("Update metric failed",
+			zap.String("MetricName", metricName),
+			zap.String("MetricType", metricType))
+		http.Error(w, fmt.Sprintf("Неизвестный тип метрики: %s", metricType), http.StatusBadRequest)
+	}
+}
+
+func (handler *MetricHandler) HandleGetMetricJson(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-type", "application/json; charset=utf-8")
+	metric := models.Metrics{}
+	easyjson.UnmarshalFromReader(r.Body, &metric)
+
+	metricType := metric.MType
+	metricName := metric.ID
+
+	if metricName == "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	switch metricType {
+	case "counter":
+		value, ok := handler.metricService.GetCounter(metricName)
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		last := value[len(value)-1]
+		metric.Delta = &last
+		easyjson.MarshalToHTTPResponseWriter(metric, w)
+	case "gauge":
+		value, ok := handler.metricService.GetGauge(metricName)
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		metric.Value = &value
+		easyjson.MarshalToHTTPResponseWriter(metric, w)
+	default:
+		w.Header().Set("Content-type", "text/plain; charset=utf-8")
+		http.Error(w, fmt.Sprintf("Неизвестный тип метрики: %s", metricType), http.StatusBadRequest)
+	}
+}
+
 func MetricRouter() chi.Router {
 	r := chi.NewRouter()
 	metricHandler := NewMetricHandler()
 	r.Route("/update", func(r chi.Router) {
-		r.Route("/", func(r chi.Router) {
-			r.Post("/{metric_type}/{metric_name}/{metric_value}", logger.RequestLogger(metricHandler.HandleUpdate))
-		})
+		r.Post("/", logger.RequestLogger(metricHandler.HandleUpdateJson))
+		r.Post("/{metric_type}/{metric_name}/{metric_value}", logger.RequestLogger(metricHandler.HandleUpdate))
 	})
 	r.Route("/value", func(r chi.Router) {
+		r.Post("/", logger.RequestLogger(metricHandler.HandleGetMetricJson))
 		r.Get("/{metric_type}/{metric_name}", logger.RequestLogger(metricHandler.HandleGetMetric))
 	})
 	r.Route("/", func(r chi.Router) {
 		r.Get("/", logger.RequestLogger(metricHandler.HandleGetMetrics))
 	})
+
 	return r
 }
--- internal/handler/metric_test.go
+++ internal/handler/metric_test.go
@@ -1,7 +1,6 @@
 package handler_test
 
 import (
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,6 +8,7 @@ import (
 	"github.com/cymiam/metircs-store/internal/handler"
 	models "github.com/cymiam/metircs-store/internal/model"
 	"github.com/go-resty/resty/v2"
+	"github.com/mailru/easyjson"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -24,31 +24,228 @@ func createTestData(t *testing.T, server *httptest.Server,
 	assert.NoError(t, err, "error making HTTP request")
 }
 
-func TestMetricHandler(t *testing.T) {
+func createTestDataJson(t *testing.T, server *httptest.Server, path string,
+	data models.Metrics) {
+
+	req := resty.New().R()
+	req.Method = "POST"
+	req.URL = server.URL + path
+
+	body, err := easyjson.Marshal(data)
+	require.NoError(t, err, "error Marshall request body")
+	req.Body = body
+
+	_, err = req.Send()
+	assert.NoError(t, err, "error making HTTP request")
+}
+
+func TestMetricHandler_Update(t *testing.T) {
 	server := httptest.NewServer(handler.MetricRouter())
 	defer server.Close()
 
-	createTestData(t, server, "/update/counter/testCounter/1")
-	createTestData(t, server, "/update/counter/testCounter2/2")
-	createTestData(t, server, "/update/counter/testCounter2/2")
-	createTestData(t, server, "/update/counter/testCounter2/2")
-	createTestData(t, server, "/update/counter/testCounter2/2")
-	createTestData(t, server, "/update/counter/testCounter2/2")
+	type want struct {
+		statusCode  int
+		contentType string
+		value       string
+	}
+
+	tests := []struct {
+		name        string
+		url         string
+		contentType string
+		method      string
+		want        want
+	}{
+		{
+			name:        "POST Gauge",
+			url:         "/update/gauge/test/3.0",
+			contentType: "text/plain; charset=utf-8",
+			method:      "POST",
+			want: want{
+				statusCode:  http.StatusOK,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:        "POST Counter",
+			url:         "/update/counter/test2/5",
+			contentType: "text/plain; charset=utf-8",
+			method:      "POST",
+			want: want{
+				statusCode:  http.StatusOK,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:        "POST Unknown metric type",
+			url:         "/update/timeseries/requests/5",
+			contentType: "text/plain; charset=utf-8",
+			method:      "POST",
+			want: want{
+				statusCode:  http.StatusBadRequest,
+				contentType: "text/plain; charset=utf-8",
+				value:       "Неизвестный тип метрики: timeseries\n",
+			},
+		},
+		{
+			name:        "POST No metric name",
+			url:         "/update/counter/5",
+			contentType: "text/plain; charset=utf-8",
+			method:      "POST",
+			want: want{
+				statusCode:  http.StatusNotFound,
+				contentType: "text/plain; charset=utf-8",
+				value:       "404 page not found\n",
+			},
+		},
+	}
+
+	for _, tc := range tests {
+
+		t.Run(tc.name, func(t *testing.T) {
+			req := resty.New().R()
+			req.Method = tc.method
+			req.URL = server.URL + tc.url
+			req.Header.Set("Content-Type", tc.contentType)
+
+			resp, err := req.Send()
+			assert.NoError(t, err, "error making HTTP request")
+
+			require.Equal(t, tc.want.statusCode, resp.StatusCode())
+			require.Equal(t, tc.want.contentType, resp.Header().Get("Content-Type"))
+
+			if tc.want.value != "" {
+				assert.Equal(t, tc.want.value, string(resp.Body()))
+			}
+		})
+
+	}
+}
+
+func TestMetricHandler_UpdateJson(t *testing.T) {
+	server := httptest.NewServer(handler.MetricRouter())
+	defer server.Close()
+
+	jsonGaugeValue := 3.1415
+	jsonCounterValue := int64(5)
+
+	type want struct {
+		statusCode  int
+		contentType string
+	}
+
+	tests := []struct {
+		name        string
+		url         string
+		body        models.Metrics
+		contentType string
+		method      string
+		want        want
+	}{
+		{
+			name:        "POST Counter JSON",
+			url:         "/update",
+			contentType: "application/json",
+			body: models.Metrics{
+				ID:    "CounterJson",
+				MType: "counter",
+				Delta: &jsonCounterValue,
+			},
+			method: "POST",
+			want: want{
+				statusCode: http.StatusOK,
+			},
+		},
+		{
+			name:        "POST Gauge JSON",
+			url:         "/update",
+			contentType: "application/json",
+			body: models.Metrics{
+				ID:    "GaugeJson",
+				MType: "gauge",
+				Value: &jsonGaugeValue,
+			},
+			method: "POST",
+			want: want{
+				statusCode: http.StatusOK,
+			},
+		},
+		{
+			name:        "POST Unknown metric type JSON",
+			url:         "/update",
+			contentType: "application/json",
+			body: models.Metrics{
+				ID:    "CounterJson",
+				MType: "timeseries",
+			},
+			method: "POST",
+			want: want{
+				statusCode:  http.StatusBadRequest,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:        "POST No metric name JSON",
+			url:         "/update",
+			contentType: "application/json",
+			body: models.Metrics{
+				MType: "counter",
+				Delta: &jsonCounterValue,
+			},
+			method: "POST",
+			want: want{
+				statusCode: http.StatusNotFound,
+			},
+		},
+	}
+
+	for _, tc := range tests {
+
+		t.Run(tc.name, func(t *testing.T) {
+			req := resty.New().R()
+			req.Method = tc.method
+			req.URL = server.URL + tc.url
+			req.Header.Set("Content-Type", tc.contentType)
+
+			if tc.contentType == "application/json" {
+				body, err := easyjson.Marshal(tc.body)
+				require.NoError(t, err, "error Marshall request body")
+				req.Body = body
+			}
+
+			resp, err := req.Send()
+			assert.NoError(t, err, "error making HTTP request")
+
+			require.Equal(t, tc.want.statusCode, resp.StatusCode())
+			require.Equal(t, tc.want.contentType, resp.Header().Get("Content-Type"))
+		})
+
+	}
+}
+
+func TestMetricHandler_Value(t *testing.T) {
+	server := httptest.NewServer(handler.MetricRouter())
+	defer server.Close()
+
+	createTestData(t, server, "/update/counter/testCounter/2")
+	createTestData(t, server, "/update/counter/testCounter/2")
+	createTestData(t, server, "/update/counter/testCounter/2")
+	createTestData(t, server, "/update/counter/testCounter/2")
+	createTestData(t, server, "/update/counter/testCounter/2")
 	createTestData(t, server, "/update/gauge/testGauge/3.14")
 
 	type want struct {
 		statusCode  int
 		contentType string
-		body        io.ReadCloser
 		value       string
 	}
 
 	tests := []struct {
-		name   string
-		url    string
-		body   models.Metrics
-		method string
-		want   want
+		name        string
+		url         string
+		contentType string
+		method      string
+		want        want
 	}{
 		{
 			name:   "GET Gauge",
@@ -61,18 +258,8 @@ func TestMetricHandler(t *testing.T) {
 			},
 		},
 		{
-			name:   "GET Single Counter",
+			name:   "GET Counter",
 			url:    "/value/counter/testCounter",
-			method: "GET",
-			want: want{
-				statusCode:  http.StatusOK,
-				contentType: "text/plain; charset=utf-8",
-				value:       "1",
-			},
-		},
-		{
-			name:   "GET Multiple Counters",
-			url:    "/value/counter/testCounter2",
 			method: "GET",
 			want: want{
 				statusCode:  http.StatusOK,
@@ -100,42 +287,125 @@ func TestMetricHandler(t *testing.T) {
 				value:       "",
 			},
 		},
+	}
+
+	for _, tc := range tests {
+
+		t.Run(tc.name, func(t *testing.T) {
+			req := resty.New().R()
+			req.Method = tc.method
+			req.URL = server.URL + tc.url
+			req.Header.Set("Content-Type", tc.contentType)
+
+			resp, err := req.Send()
+			assert.NoError(t, err, "error making HTTP request")
+
+			require.Equal(t, tc.want.statusCode, resp.StatusCode())
+			require.Equal(t, tc.want.contentType, resp.Header().Get("Content-Type"))
+
+			if tc.want.value != "" {
+
+				assert.Equal(t, tc.want.value, string(resp.Body()))
+			}
+		})
+
+	}
+}
+
+func TestMetricHandler_ValueJson(t *testing.T) {
+	server := httptest.NewServer(handler.MetricRouter())
+	defer server.Close()
+
+	jsonGaugeValue := 3.1415
+	jsonCounterValue := int64(5)
+
+	createTestDataJson(t, server, "/update", models.Metrics{
+		MType: "counter",
+		ID:    "testCounterJson",
+		Delta: &jsonCounterValue,
+	})
+
+	createTestDataJson(t, server, "/update", models.Metrics{
+		MType: "gauge",
+		ID:    "testGaugeJson",
+		Value: &jsonGaugeValue,
+	})
+
+	type want struct {
+		statusCode  int
+		contentType string
+		body        string
+	}
+
+	tests := []struct {
+		name        string
+		url         string
+		body        models.Metrics
+		contentType string
+		method      string
+		want        want
+	}{{
+		name:        "GET Counter JSON",
+		url:         "/value",
+		method:      "POST",
+		contentType: "application/json",
+		body: models.Metrics{
+			ID:    "testCounterJson",
+			MType: "counter",
+		},
+		want: want{
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			body: `{
+				"id":    "testCounterJson",
+				"type": "counter",
+				"delta": 5}`,
+		},
+	},
 		{
-			name:   "POST Gauge",
-			url:    "/update/gauge/test/3.0",
-			method: "POST",
+			name:        "GET Gauge JSON",
+			url:         "/value",
+			method:      "POST",
+			contentType: "application/json",
+			body: models.Metrics{
+				ID:    "testGaugeJson",
+				MType: "gauge",
+			},
 			want: want{
 				statusCode:  http.StatusOK,
-				contentType: "text/plain; charset=utf-8",
+				contentType: "application/json",
+				body: `{
+				"id":    "testGaugeJson",
+				"type": "gauge",
+				"value": 3.1415}`,
 			},
 		},
 		{
-			name:   "POST Counter",
-			url:    "/update/counter/test2/5",
-			method: "POST",
-			want: want{
-				statusCode:  http.StatusOK,
-				contentType: "text/plain; charset=utf-8",
+			name:        "GET Unknown Counter JSON",
+			url:         "/value",
+			method:      "POST",
+			contentType: "application/json",
+			body: models.Metrics{
+				ID:    "unknownCounter",
+				MType: "counter",
 			},
-		},
-		{
-			name:   "POST Unknown metric type",
-			url:    "/update/timeseries/requests/5",
-			method: "POST",
-			want: want{
-				statusCode:  http.StatusBadRequest,
-				contentType: "text/plain; charset=utf-8",
-				value:       "Неизвестный тип метрики\n",
-			},
-		},
-		{
-			name:   "POST No metric name",
-			url:    "/update/counter/5",
-			method: "POST",
 			want: want{
 				statusCode:  http.StatusNotFound,
-				contentType: "text/plain; charset=utf-8",
-				value:       "404 page not found\n",
+				contentType: "application/json; charset=utf-8",
+			},
+		},
+		{
+			name:        "GET Unknown Gauge JSON",
+			url:         "/value",
+			method:      "POST",
+			contentType: "application/json",
+			body: models.Metrics{
+				ID:    "unknownGauge",
+				MType: "counter",
+			},
+			want: want{
+				statusCode:  http.StatusNotFound,
+				contentType: "application/json; charset=utf-8",
 			},
 		},
 	}
@@ -146,14 +416,23 @@ func TestMetricHandler(t *testing.T) {
 			req := resty.New().R()
 			req.Method = tc.method
 			req.URL = server.URL + tc.url
+			req.Header.Set("Content-Type", tc.contentType)
+
+			if tc.contentType == "application/json" {
+				body, err := easyjson.Marshal(tc.body)
+				require.NoError(t, err, "error Marshall request body")
+				req.Body = body
+			}
 
 			resp, err := req.Send()
 			assert.NoError(t, err, "error making HTTP request")
-			assert.Equal(t, tc.want.statusCode, resp.StatusCode())
-			assert.Equal(t, tc.want.contentType, resp.Header().Get("Content-Type"))
 
-			if resp.Header().Get("Content-Type") == "text/plain; charset=utf-8" {
-				require.Equal(t, tc.want.value, string(resp.Body()))
+			require.Equal(t, tc.want.statusCode, resp.StatusCode())
+			require.Equal(t, tc.want.contentType, resp.Header().Get("Content-Type"))
+
+			if tc.want.body != "" {
+
+				assert.JSONEq(t, tc.want.body, string(resp.Body()))
 			}
 		})
 
